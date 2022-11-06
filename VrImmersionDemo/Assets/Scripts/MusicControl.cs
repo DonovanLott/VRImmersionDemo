c275e392c62a28331d756dba4c819f6271ce02ed
Anthony01P <113856290+Anthony01P@users.noreply.github.com>
2022-11-06 00:12:22 -0400

    Add files via upload

---
+++ VrImmersionDemo/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MusicControl : MonoBehaviour
+{
+    public AudioSource audioObject;
+    public bool closeEnough;
+    public int colliderCount;
+
+    void Update()
+    {
+        if (closeEnough == true)
+        {
+            if (!audioObject.isPlaying)
+            {
+                audioObject.Play();
+            }
+        }
+        else if (closeEnough == false)
+        {
+            if (audioObject.isPlaying)
+            {
+                audioObject.Pause();
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            colliderCount += 1;
+            Debug.Log("Player has approached the shrine.");
+            UpdateState();
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            colliderCount -= 1;
+            Debug.Log("Player is leaving the shrine.");
+            UpdateState();
+        }
+    }
+    void UpdateState()
+    {
+        if (colliderCount == 0)
+        {
+            closeEnough = false;
+        }
+        else if (colliderCount > 0)
+        {
+            closeEnough = true;
+        }
+    }
+}
